6ba9ee780ffefe8128db3eb32d212683c23b6ecc
Sasen Cain <sasen@mit.edu>
2015-05-03 15:02:46 -0700

    gitignore .mat; starting to analyzing trialBlocks

---
+++ analysis/loadBlock.m
@@ -0,0 +1,28 @@
+clear all;
+datafile = '../DATA/het888/eq12_55_60.mat';
+load(datafile)
+
+varnames = fieldnames(expdata);
+for var = 1:length(varnames)
+  vname = varnames{var};
+  thisval = getfield(expdata,vname);
+  assignin('base',vname,thisval);
+end
+%cellstr(veridical')
+
+gotWrong = []; gotRight = [];
+for tr = 1:length(Lmean)
+  truth(tr) = strcmp(veridical(tr),'l');
+  if ACCs(tr) == 0
+    gotWrong(end+1,:) = [tr Lmean(tr) Rmean(tr) Lmean(tr)/Rmean(tr)];
+  elseif ACCs(tr) == 1
+    gotRight(end+1,:) = [tr Lmean(tr) Rmean(tr) Lmean(tr)/Rmean(tr)];
+  end
+end
+
+ratrange = minmax(Lmean./Rmean);
+histbins = linspace(ratrange(1),ratrange(2),20)';
+histRight = histc(gotRight(:,end),histbins);
+histWrong = histc(gotWrong(:,end),histbins);
+
+bar(histbins,[histRight histWrong],'histc')
