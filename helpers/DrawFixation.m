bc6e212478cd440c75ed68be92fc02e5e190be37
Sasen Cain <sasen@mit.edu>
2015-05-12 17:42:35 -0700

    fixation cross is now an X, and there's a giant line down the middle

--- helpers/DrawFixation.m
+++ helpers/DrawFixation.m
@@ -1,6 +1,6 @@
 function [] = DrawFixation(window, armLength, x, y, rgb)
 % function [] = DrawFixation(window, armLength, x, y, rgb)
-% Draw fixation cross (+) to window.
+% Draw fixation cross (x) and LR-split line to window.
 %    window: destination PsychToolbox window (with background already drawn)
 %    armLength: size in pixels
 %    x, y: position of the cross's center on window, in pixels
@@ -11,5 +11,8 @@ if nargin == 4
   rgb = [255 255 255]; % default to white cross
 end
 
-Screen('DrawLine', window, rgb, x - armLength, y, x + armLength, y);
-Screen('DrawLine', window, rgb, x, y - armLength, x, y + armLength);
+%Screen('DrawLine', window, rgb, x - armLength, y, x + armLength, y);
+Screen('DrawLine', window, rgb, x - armLength, y+armLength, x + armLength, y-armLength);
+%Screen('DrawLine', window, rgb, x, y - armLength, x, y + armLength);
+Screen('DrawLine', window, rgb, x-armLength, y - armLength, x+armLength, y + armLength);
+Screen('DrawLine', window, rgb, x, 1, x, 800);
